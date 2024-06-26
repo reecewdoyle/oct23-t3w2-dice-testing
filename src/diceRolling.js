7210ebd78e5aa743e72c90171e065f373cff7fe5
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-06-26 19:15:32 +1000

    docs: add docs to dice rolls

--- src/diceRolling.js
+++ src/diceRolling.js
@@ -1,5 +1,13 @@
 
 
+/**
+ * Generate a random number between 1 and a given size. 
+ * @date 26/06/24 - 11:42:29 AM
+ * @author ReeceDoyle
+ * 
+ * @param [diceSize=6] - Number representing the size of the dice you want roll.
+ * @returns A random number between 1 and a given size
+ */
 function rollDice(diceSize = 6){
     let rollResult= Math.floor(Math.random() * diceSize) + 1;
 
@@ -7,6 +15,14 @@ function rollDice(diceSize = 6){
 }
 
 
+/**
+ * Generate a random number between 1 and a given size twice, and identfifying the highest number.
+ * @date 26/06/24 - 11:42:29 AM
+ * @author ReeceDoyle
+ * 
+ * @param [diceSize=6] - Number representing the size of the dice you want roll.
+ * @returns A random number between 1 and a given size twice, and indentifying the highest numnber.
+ */
 function rollDiceWithAdvantage(diceSize =6){
     let result = {
         finalResult: null,
@@ -26,7 +42,14 @@ function rollDiceWithAdvantage(diceSize =6){
 }
 
 
-
+/**
+ * Generate a random number between 1 and a given size twice, and identfifying the lowest number.
+ * @date 26/06/24 - 11:42:29 AM
+ * @author ReeceDoyle
+ * 
+ * @param [diceSize=6] - Number representing the size of the dice you want roll.
+ * @returns A random number between 1 and a given size twice, and indentifying the lowest numnber.
+ */
 function rollDiceWithDisadvantage(diceSize =6){
     let result = {
         finalResult: null,
